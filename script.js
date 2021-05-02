4bc58ec7db645bb07e41ef4517fcb47a039af8b0
Justin <pepsivanilla93@gmail.com>
2021-05-01 22:20:08 -0400

    added 5 game repeat functionality and finished the project

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 function computerPlay() {
   let num = Math.random();
   if (num < 0.33) {
-    return 'Rock';
+    return 'rock';
   } else if (0.33 <= num && num < 0.66) {
-    return 'Paper';
+    return 'paper';
   } else {
-    return 'Scissors';
+    return 'scissors';
   }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === "rock") {
+    if (computerSelection === "scissors") {
+      return "You win! Rock beats Scissors";
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+      return "Tie. You both chose Rock";
+    } else {
+      return "You Lose! Paper beats Rock";
+    }
+  }
+  if (playerSelection.toLowerCase() === "paper") {
+    if (computerSelection === "rock") {
+      return "You win! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+      return "Tie. You both chose Paper";
+    } else {
+      return "You Lose! Scissors beats Paper";
+    }
+  }
+  if (playerSelection.toLowerCase() === "scissors") {
+    if (computerSelection === "paper") {
+      return "You Win! Scissors beats Paper"
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+      return "Tie. You both chose Scissors";
+    } else {
+      return "You Lose! Rock beats Scissors";
+    }
+  }
+  return "";
+}
+
+function game() {
+  let playerSelection = prompt("Rock, Paper, Scissors?");
+  let computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+}
+
+//const computerSelection = computerPlay();
+
+game();
+game();
+game();
+game();
+game();
